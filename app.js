91b1e1c6efbf424644ff207ab614676b3a08c649
saja-simple-website <80650364+saja-simple-website@users.noreply.github.com>
2021-03-29 22:37:43 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,73 +1,99 @@
-const express = require('express')
-const { sequelize , User , Post} = require('./models')
-const app = express()
-app.use(express.json())
+const {sequelize} = require('./models');
+const user =require('./models');
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const userRoute =('./route/userRoute');
+const postRoute =('./route/postRoute');
+const commentRoute =('./route/commentRoute');
+const reactionRoute =('./route/reactionRoute');
 
-app.post('/users',async(req,res)=>{
-    const {name, email ,role} = req.body
-    try{
-        const user = await User.create ({name,email,role})
-        return res.json(user)
-    } catch(err){
-        console.log(err)
-        return res.status(500).json (err)
-    }
-})
-app.delete('/users/:uuid', async (req,res) =>{
-    const uuid =req.params.uuid
-    try{
-        const user = await User.findOne({ where:{uuid}})
-        await user.destroy()
-            return res.json({message:'User deleted!'})
-    }catch(err){
-        console.log(err)
-        return res.status(500).json(err)
-    }
-    })
-    app.put('/users/uuid',async (req,res)=>{
-        const uuid = req.params.uuid
-        const{name, email,role}= req.body
-        try{
-            const user = await User.findOne({where:{uuid}})
-            return res.json(user)
-            user.name = name
-            user.email=email
-            user.role = role
-            await user.save()
-            return res.json(user)
-        }catch(err){
-            console.log(err)
-            return res.status(500).json(err)
+const app = express();
+
+app.get('/api',(req,res) => {
+    res.json({
+        message:'welcome to the API'
+    });
+});
+
+app.post ('/api/posts',verifyToken,(req,res)=>{
+    jwt.verify(req.token,'secretkey',(err,authData)=>{
+        if (err){
+            res.sendStatus(403);
+        }else{
+              res.json({
+        message:'Post created....',
+        authData
+    });
         }
-        })
 
-app.get('/users', async (req,ers)=>{
-    try{
-        const users = await User.findAll({include:[{model:User,as:'user'}]})
-        return res.json(users)
-    } catch (err){
-        console.log(err)
-        return res.status(500).json({ error:'Something went wrong'})
+    });
+  
+});
+
+app.post ('/api/login',(req,res)=>{
+    //Mock user
+    const user={
+        id:1,
+        username:'sajaalquran',
+        email:'sajaalquran@gmail.com'
     }
-})
-app.post('/posts', async (req,res) =>{
-const {UserUuid ,body} =req.body
-try{
-    const user = await User.findOne(
-        {where:{uuid},
-    include:('posts')
-})
-const post = await Post.create({ body, userId:user.id})
-return res.json(post)
-} catch(err){
-    console.log(err)
-    return res.status(500).json(err)
-}
-})
+    jwt.sign({user},'secretkey',{expiresIn:'30s'},(err,token )=>{
+        res.json({
+            token
+        });
+    });
 
-app.listen({port:5000}, async()=>{
-    console.log('Server up on http://localhost:5000')
-    await sequelize.authenticate()
-    console.log('Database Connected!')
-})
+});
+
+//verify Token 
+function verifyToken (req,res,next){
+    //Get auth header value
+    const bearerHeader = req.headers['authorization'];
+    if(typeof bearerHeader !=='undefined'){
+        const bearer = bearerHeader.split('');
+        console.log(bearer);
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    }
+        else{
+            res.endStatus(403);
+        }
+    
+}
+//verify Acess Token 
+async function verifyAcessToken (req,res,next){
+    try{
+        const UserToken= req.headers.authorization;
+        if(!UserToken) 
+        return res.send({message:'no token provide'});
+        const accessToken = authToken.split('')[1];
+        if (!accessToken) 
+        return res.send({message:'no token'});
+        jwt.verifyAccessToken(accessToken);
+       
+        next();}
+        catch (err){
+            next(err);
+        }
+//verify refresh Token 
+async function verifyRefreshToken (refreshToken){
+    UserToken.find({user:userId}).sort({createdAt:-1}).limit(1);
+    userToken= userToken [0];
+    if(!userToken)
+        return res.send({message:'User token does not exist'});
+        if(UserToken.refreshToken !==refreshToken)
+        return res.send({message:'not valid'});
+        UserToken.findOne ({_id:userToken._id},{refr:{refreshToken:refToken}});
+        return {accessToken:accessToken,refreshToken:refToken};
+}
+
+    }
+    async function checkIfAllowed(userId){
+        const userToken = await userToken.find({user:userId}).sort({createdAt:-1}).limit(1);
+        if (!userToken[0]) throw{isError:true,message:'User token does not exist'}
+    }
+
+app.listen(5000,()=>console.log('Server started on port 5000'));
+
 
